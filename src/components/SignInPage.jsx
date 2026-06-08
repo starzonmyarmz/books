257a4663e7f2f86e244012b72f0f5ef81c0a55e8
Daniel Marino <iamdanielmarino@gmail.com>
2026-06-08 18:45:01 -0400

    add heading to sign in

--- src/components/SignInPage.jsx
+++ src/components/SignInPage.jsx
@@ -3,6 +3,7 @@ import { signIn } from "../auth.js"
 export function SignInPage() {
   return (
     <main id="sign-in">
+      <h1>My Bookshelf</h1>
       <button class="btn btn-google" onClick={signIn}>
         Sign in with Google
       </button>
